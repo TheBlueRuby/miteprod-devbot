b1e4aed9978220698b89f99da4d5e5c202157c8e
TheBlueRuby <109350270+TheBlueRuby@users.noreply.github.com>
2022-10-24 00:10:08 +1100

    now requests miteprod api for data

--- commands/games.js
+++ commands/games.js
@@ -1,5 +1,49 @@
 const { SlashCommandBuilder } = require('discord.js');
 
+async function createGamesEmbed () {
+    let apiData = await updateGameData();
+
+    let gameEmbedData = [];
+
+    apiData.forEach((game) => {
+        let gameEntry = {
+            name: game.displayName,
+            value: 'by ' + game.author + '\n\n' + game.desc + '\n'
+        };
+        gameEmbedData.push(gameEntry);
+        console.log(gameEntry);
+    });
+
+    console.log('\n' + gameEmbedData);
+    return gameEmbedData;
+
+}
+
+async function updateGameData() {
+    let apiUrl = "https://miteprod-api.vercel.app/";
+
+    let gamesListObj = await grabData(apiUrl + "games/");
+    let gamesList = gamesListObj.games;
+
+    let games = [];
+
+    for (let i = 0; i < gamesList.length; i++) {
+        games[i] = await grabData(apiUrl + "games/" + gamesList[i] + "/");
+    }
+
+    return games;
+}
+
+async function grabData(apiUrl) {
+    try {
+        let res = await fetch(apiUrl);
+        return await res.json();
+    } catch (error) {
+        console.error(error);
+        return error;
+    }
+}
+
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('games')
@@ -12,16 +56,7 @@ module.exports = {
             thumbnail: {
                 url: 'https://cdn.discordapp.com/app-icons/924524926264807435/4e759389292fcd35e458bed2d95a1188.png?size=1024'
             },
-            fields: [
-                {
-                    name: 'TheBlueRuby',
-                    value: 'Backrooms First Contact (Windows)'
-                },
-                {
-                    name: 'BixBox',
-                    value: 'Backrooms First Contact (Scratch)'
-                }
-            ],
+            fields: await createGamesEmbed(),
 
         };
 		await interaction.reply({embeds: [gameEmbed]});
