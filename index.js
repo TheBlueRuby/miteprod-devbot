69b4b191c251ed7e13d7b74d92dbd9fb02618ba3
TheBlueRuby <109350270+TheBlueRuby@users.noreply.github.com>
2022-10-24 01:31:35 +1100

    using dotenv

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const fs = require('node:fs');
 const path = require('node:path');
 
 const { Client, Collection, GatewayIntentBits } = require('discord.js');
-const { token } = require('./config.json');
+
+require('dotenv').config()
+const token = process.env.DISCORD_TOKEN;
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
